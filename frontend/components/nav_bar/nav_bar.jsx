823ac5cfde21427e507c465e6eb655c0bce426ac
Freddy Rendon <frendonjr03@gmail.com>
2022-10-05 20:56:17 -0400

    added logout button

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -33,52 +33,13 @@ class navBar extends React.Component {
                     <Link to="/signup" className="signup-login-link">Sign Up</Link>
                         </>
                     }
+                
+                    <div>
+                        <button className="btn create form-logout" onClick={logout}>Log Out</button>
+                    </div>
                 </div>
             </>
         )
-
-
-        // const personalGreeting = () => ( // can have this render splash page component instead
-        //     <>
-        //         <div className="navBar">
-        //             <Link to='/' className={"logo"} >
-        //                 ravebrite
-        //             </Link>
-
-        //             {/* <div className="nav-search-bar-wrapper">
-        //         <input type="text" className="nav-search-bar"/>
-        //       </div> */}
-
-        //             <div className="nav-bar-links">
-        //                 <Link to="/events/create" className="create link-button">
-        //                     <i className="fas fa-plus-circle link-image"></i>
-        //                     Create Event
-        //                 </Link>
-        //                 <Link to={`/users/${currentUser.id}/bookmarks`} className="link-button">
-        //                     <i className="far fa-heart link-image"></i>
-        //                     Likes
-        //                 </Link>
-        //                 <Link to={`/users/${currentUser.id}/registrations`} className="link-button">
-        //                     <i className="fas fa-ticket-alt link-image"></i>
-        //                     Tickets
-        //                 </Link>
-        //                 <div className="dropdown">
-        //                     <a className="signup-login-link dropbtn">    {/* change this to Link when I create userShow --> */}
-        //                         <i className="far fa-smile profilePic"></i>
-        //                         {currentUser.email}
-        //                     </a>
-        //                     <div className="dropdown-content">
-        //                         <Link to={`/users/${currentUser.id}/events`}>
-        //                             Manage my events
-        //                         </Link>
-        //                         <a onClick={logout}>Log Out</a>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </>
-        // );
-
 
 
         return (
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import usersReducer from "./user_reducer";
 import eventsReducer from "./events_reducer"
+import likesReducer from "./likes_reducer";
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
-    events: eventsReducer
+    events: eventsReducer,
+    likes: likesReducer
 })
 
 export default entitiesReducer
