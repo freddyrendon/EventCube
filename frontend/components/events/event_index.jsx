bf47cfcdd6fd0794e76a62da31ee28b050983ca5
Freddy Rendon <frendonjr03@gmail.com>
2023-01-23 00:11:47 -0500

    adjusted cards

--- frontend/components/events/event_index.jsx
+++ frontend/components/events/event_index.jsx
@@ -12,6 +12,7 @@ function EventIndex(props) {
     const events = props.events.map(event => { 
         return <div className="events-loco" key={event.id}
             onClick={() => { window.location.href = `/#/events/${event.id}` }}>
+                <img className="momo-pic" src="https://www.djtimes.com/wp-content/uploads/2021/08/electric-zoo-music-festival-reveals-set-times-for-2021-dj-times.jpg" alt="" />
             <div className="momo">
                 {event.event_title}
                 <br />
@@ -19,7 +20,6 @@ function EventIndex(props) {
                  <br />
                  <div>
                  </div>
-                <img className="momo-pic" src="https://www.djtimes.com/wp-content/uploads/2021/08/electric-zoo-music-festival-reveals-set-times-for-2021-dj-times.jpg" alt="" />
             </div>
 
                  
