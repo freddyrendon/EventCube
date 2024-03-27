f4e6ad50416eb1368b0acfe4b27b10b3cd9cf355
Freddy Rendon <frendonjr03@gmail.com>
2024-03-27 16:32:39 -0400

    fixed text seed file

--- app/views/api/events/index.json.jbuilder
+++ app/views/api/events/index.json.jbuilder
@@ -6,3 +6,13 @@
         end
     end
 # end 
+
+# json.array! @events do |event|
+#   json.extract! event, :id, :event_title, :event_body, :location, :event_start_date, :event_start_time
+
+#   if event.photo.attached?  # Assuming 'photo' is the name of the attachment
+#     json.attachment do
+#       json.url url_for(event.photo)  # Render the URL of the attached photo
+#     end
+#   end
+# end
--- db/seed.rb
+++ db/seed.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 Event.destroy_all
 puts "destroying all Events"
 User.destroy_all
@@ -15,115 +16,93 @@ puts "destroying all Users"
 demo = User.create!(first_name: "demo", last_name: "test", email:"demo@user.com", password:"demouser")
 freddy = User.create!(first_name: "Freddy", last_name: "Rendon", email:"FreddyRendon@aio.com", password:"password")
 
+
+
 Event.create!(
                 host_id: demo.id,
                 category_id: 1,
-                event_capacity: 500,
                 event_title: "Electric Zoo Festival",
                 event_body: "This is the biggest festival of the year in NYC.",
                 location: "Randall's Island Park",
                 event_start_date: "2022-09-02",
-                event_end_date: "2022-09-04",
                 event_start_time: "15:00:00",
-                event_end_time: "23:00:00"),
+                # photo: "https://www.billboard.com/wp-content/uploads/media/ezoo-atmosphere-2019-billboard-1548.jpg")
+)
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 250,
+                category_id: 2,
                 event_title: "New York Yankees",
                 event_body: "Dont miss out on this ultimate rivalry Boston Red Sox vs The New York Yankees",
                 location: "The Bronx, NY",
-                category_id: 2,
                 event_start_date: "2022-09-02",
-                event_end_date: "2022-09-02",
-                event_start_time: "18-00-00",
-                event_end_time: "20-00-00"),
+                event_start_time: "18-00-00")
 
             
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 3,
                 event_title: "Wonder Pig Korean BBQ",
                 event_body: "Bringing an AYCE (All You Can Eat) K-BBQ experience",
                 location: "LIC, Queens",
-                category_id: 3,
                 event_start_date: "2022-09-02",
-                event_end_date: "2022-09-02",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00"),
+                event_start_time: "12:00:00")
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 300,
+                category_id: 1,
                 event_title: "Bad Bunny Baby",
                 event_body: "Picante picante como un habanero!",
                 location: "Queens, NY",
-                category_id: 1,
                 event_start_date: "2022-09-05",
-                event_end_date: "2022-09-05",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00"),
+                event_start_time: "12:00:00")
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 4,
                 event_title: "230 5th rooftop",
                 event_body: "Dance the night away",
                 location: "Manhattan, NY",
-                category_id: 4,
                 event_start_date: "2022-09-12",
-                event_end_date: "2022-09-12",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00")
+                event_start_time: "12:00:00")
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 3,
                 event_title: "Williamsburg Bites Brooklyn Food Tour",
                 event_body: "Explore and taste your way through Brooklyn",
                 location: "Brooklyn, NY",
-                category_id: 3,
                 event_start_date: "2022-09-08",
-                event_end_date: "2022-09-08",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00")
+                event_start_time: "12:00:00")
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 1,
                 event_title: "Rammstein",
                 event_body: "du...du hast...du hast mich",
                 location: "East Rutherford, NJ",
-                category_id: 1,
                 event_start_date: "2022-09-11",
-                event_end_date: "2022-09-11",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00")
+                event_start_time: "12:00:00")
 
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 2,
                 event_title: "rpm raceway",
                 event_body: "RPM Raceway is the ultimate all-electric indoor go-kart",
                 location: "East Farmingdale, New York",
-                category_id: 2,
                 event_start_date: "2022-09-02",
-                event_end_date: "2022-09-02",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00")                
+                event_start_time: "12:00:00")
 
 Event.create!(
                 host_id: demo.id,
-                event_capacity: 100,
+                category_id: 4,
                 event_title: "Mr purple",
                 event_body: "On the Hotel Indigo's 15th floor",
                 location: "Manhattan, NY",
-                category_id: 4,
                 event_start_date: "2022-09-01",
-                event_end_date: "2022-09-01",
-                event_start_time: "12:00:00",
-                event_end_time: "23:00:00")
+                event_start_time: "12:00:00"
+            )
 
 
             ## img filed seed to events 
